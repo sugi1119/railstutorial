cbbb386274e57821ce51aa20df0efc4a66b84d51
Yuko Sugiyama <sugi_yuko@hotmail.com>
2015-05-21 12:37:28 +1000

    Add test helper

--- sample_app/test/integration/site_layout_test.rb
+++ sample_app/test/integration/site_layout_test.rb
@@ -9,5 +9,15 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+  end
+
+  test "signup links" do
+    get signup_path
+    assert_template 'users/new'
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "title", full_title("Sign up")
   end
 end
